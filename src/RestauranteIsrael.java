d9feedb264d34b38cfca887f04b6b3ae1b0a85f8
Aurora <daw13.2022@gmail.com>
2022-02-26 17:14:53 +0100

    Código Documentado

--- src/RestauranteIsrael.java
+++ src/RestauranteIsrael.java
@@ -1,8 +1,23 @@
 
+/**
+ * La clase RestauranteIsrael gestiona los comensales del restaurante Israel
+ * @author Aurora Tejada Cordón
+ *
+ */
 public class RestauranteIsrael {
+	/**
+	 * Representa la cantidad de papa
+	 */
 	public int p;
+	/**
+	 * Representa la cantidad de chocos
+	 */
 	public int c;
-	
+	/**
+	 * Constructor de la clase Restaurante Israel
+	 * @param a Introducir la cantidad de papas que hay en stock
+	 * @param b Introducir la cantidad de chocos que hay en stock
+	 */
 	public RestauranteIsrael(int a, int b) {
 		this.p = a;
 		this.c = b;
@@ -17,15 +32,24 @@ public class RestauranteIsrael {
 		int ch = this.c * 6;
 		return ch;
 	}
-	
+	/**
+	 * Añade una cantidad de chocos al stock actual 
+	 * @param c Cantidad de chocos para añadir al stock
+	 */
 	public void addChocos(int c) {
 		this.c = this.c + c;
 	}
-	
+	/**
+	 * Añade una cantidad de papas al stock actual
+	 * @param d Cantidad de papas para añadir al stock
+	 */
 	public void addPapas(int d) {
 		this.p = this.p + d;
 	}
-	
+	/**
+	 * 
+	 * @hidden
+	 */
 	public static void main(String[] args) {
 		RestauranteIsrael r1 = new RestauranteIsrael(50, 60);
 		System.out.println("Numero de chocos: " + r1.c);
